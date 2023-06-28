d77fde6e0a83d623de70b36859e5e0ddee6e5f10
Alexey Dobrynin <ag.dobrynin@gmail.com>
2023-06-28 11:47:21 +0400

    feat: Notification for user with ShouldQueue.

--- app/Notifications/EventReminderNotification.php
+++ app/Notifications/EventReminderNotification.php
@@ -5,10 +5,11 @@ namespace App\Notifications;
 use App\Models\Event;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EventReminderNotification extends Notification
+class EventReminderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
